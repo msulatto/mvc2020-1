d7356c62adf8c517d7faf6ccebe3a140e4b35b16
msulatto <msulatto@gmail.com>
2020-07-03 20:33:21 -0300

    Create Page404.php

---
+++ app/core/Page404.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace MVC2020\Core;
+
+class Page404
+{
+    public function index()
+    {
+        echo '<h1>erro 404 PÁGINA NÃO FOI ENCONTRADA! :(((( </h1>';
+        echo "<img src='https://www.pngkey.com/png/detail/212-2124171_404-error-404-pagina-no-encontrada.png'>";
+    }
+}
