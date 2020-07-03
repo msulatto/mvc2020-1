aaae684afd253f691a2c9859ae83191001061f9f
msulatto <msulatto@gmail.com>
2020-07-03 20:47:05 -0300

    Page404 atualizado

--- app/core/Page404.php
+++ app/core/Page404.php
@@ -7,7 +7,14 @@ class Page404
 {
     public function index()
     {
-        echo '<h1>erro 404 PÁGINA NÃO FOI ENCONTRADA! :(((( </h1>';
-        echo "<img src='https://www.pngkey.com/png/detail/212-2124171_404-error-404-pagina-no-encontrada.png'>";
+
+        $nome = 'marcelli'; 
+        echo $conteudo = <<<ETEC
+        <h1>boa noite {$nome}</<h1>
+        <h1>erro 404 PÁGINA NÃO FOI ENCONTRADA! :(((( </h1>
+        <div style='text-align: center'>
+        <img src='https://www.pngkey.com/png/detail/212-2124171_404-error-404-pagina-no-encontrada.png'>
+        </div>
+ETEC;
     }
 }
